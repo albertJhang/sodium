5a653266d7b188b10144087464a8e25820fdb653
albert <hhhhhadslacer@gmail.com>
2023-11-07 13:30:49 +0800

    製作可進行四則運算的編譯器

--- sodium.c
+++ sodium.c
@@ -4,6 +4,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+//
+//Tokenizer
+//
 
 //標記種類
 typedef enum{
@@ -87,6 +90,7 @@ Token *new_token(TokenKind kind,Token *cur,char *str){
     cur->next=tok;
     return tok;
 }
+
 //將輸入文字列p作標記解析並回傳標記連結串列
 Token *tokenize(){
     char *p = user_input;
@@ -101,7 +105,8 @@ Token *tokenize(){
             continue;
         }
         
-        if(*p=='+'||*p=='-'){
+        //標點符號
+        if(strchr("+-*/()", *p)){
             cur=new_token(TK_RESERVED,cur,p++);
             continue;
         }
@@ -112,46 +117,151 @@ Token *tokenize(){
             continue;
         }
         
-        error_at(p, "預計有一個數字");
+        error_at(p, "令牌無效");
     }
     
     new_token(TK_EOF,cur,p);
     return head.next;
 }
 
+//
+//解析器
+//
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整數
+} NodeKind;
+
+//AST 節點類型
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; //節點種類
+    Node *lhs;     //左手邊
+    Node *rhs;     //右手邊
+    int val;       //如果 kind == ND_NUM 則使用
+};
+
+Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_num(int val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+//expr = mul("+" mul | "-" mul)*
+Node *expr(){
+    Node *node = mul();
+
+    for(;;) {
+       if (consume('+'))
+            node = new_binary(ND_ADD, node, mul());
+       else if(consume('-'))
+            node = new_binary(ND_SUB, node, mul());
+       else
+            return node;
+    }
+}
+
+//mul = primary("*" primary | "/" primary)*
+Node *mul() {
+    Node *node = primary();
+
+    for(;;) {
+        if(consume('*'))
+            node = new_binary(ND_MUL, node, primary());
+        else if(consume('/'))
+            node = new_binary(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+//primary = "(" expr ")" | num
+Node *primary() {
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_num(expect_number());
+}
+
+//
+//Code generator程式碼產生器
+//
+
+void gen(Node *node) {
+    if(node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return ;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch(node->kind) {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("   imul rax, rdi\n ");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc,char **argv)
 {
     if(argc !=2)
-    {
-        error("引數數量錯誤");
-        return 1;
-    }
-
+        error("%s: 參數數量無效", argv[0]);
     //標記解析
     user_input = argv[1];
-    token=tokenize();
-
+    token = tokenize();
+    Node *node = expr();
+    
     //輸出前半部份組合語言指令
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    //確認算式必須以數開頭
-    //輸出最初的mov指令
-    printf("    mov rax,    %d\n",  expect_number());
-    
-    //一邊消耗'+<數>'或'-<數>'的標記
-    //並輸出組合語言指令
-    while(!at_eof()){
-        if(consume('+')){
-            printf("    add rax,    %d\n",  expect_number());
-            continue;
-        }
+    // 遍歷 AST 以發出程式集。
+    gen(node);
 
-        expect('-');
-        printf("    sub rax,    %d\n",  expect_number());
-    }
-
+    // 結果必須位於堆疊頂部，因此將其彈出
+    // 到 RAX 使其成為程式退出程式碼。
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
