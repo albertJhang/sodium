487fbcbac15b951ed4d47820dfd8bfd5856c740f
albert <hhhhhadslacer@gmail.com>
2023-11-08 22:12:31 +0800

    新增“返回”語句

--- tokenize.c
+++ tokenize.c
@@ -70,6 +70,14 @@ static bool startswith(char *p, char *q) {
   return strncmp(p, q, strlen(q)) == 0;
 }
 
+static bool is_alpha(char c) {
+  return('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
+static bool is_alnum(char c) {
+  return is_alpha(c) || ('0' <= c && c <= '9');
+}
+
 // Tokenize `user_input` and returns new tokens.
 Token *tokenize(void) {
   char *p = user_input;
@@ -82,6 +90,13 @@ Token *tokenize(void) {
       p++;
       continue;
     }
+
+    //關鍵字Keywords
+    if (startswith(p, "return") && !is_alnum(p[6])) {
+      cur = new_token(TK_RESERVED, cur, p, 6);
+      p += 6;
+      continue;
+    } 
 
     // Multi-letter punctuators
     if (startswith(p, "==") || startswith(p, "!=") ||
