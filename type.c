3d831ec166c558a1560ba887dfca78828cfeba4c
albert <hhhhhadslacer@gmail.com>
2023-11-14 15:55:39 +0800

    實現通常的算術轉換 codegen.c 當我們將 char 或 Short 值載入到暫存器時，我們總是 將它們擴展到 int 的大小，因此我們可以假設下半部分 暫存器始終包含有效值。 的上半部分 char、short 和 int 暫存器可能包含垃圾。 當我們加載時 一個長整數值到暫存器中，它只是佔據整個暫存器。 type.c 對於許多二元運算符，我們隱式提升操作數，以便 兩個操作數具有相同的類型。 任何小於的整數類型 int 總是提升為 int。 如果一個操作數的類型較大 與另一個相比(例如“long”與“int”)，較小的操作數將 被提升以與對方匹配。 這種操作稱為「普通算術轉換」。

--- type.c
+++ type.c
@@ -19,7 +19,6 @@ bool is_integer(Type *ty) {
   TypeKind k = ty->kind;
   return k == TY_CHAR || k == TY_SHORT || k == TY_INT ||
          k == TY_LONG;
-  return k == TY_CHAR || k == TY_INT || k == TY_LONG;
 }
 
 Type *copy_type(Type *ty) {
@@ -48,6 +47,27 @@ Type *array_of(Type *base, int len) {
   return ty;
 }
 
+static Type *get_common_type(Type *ty1, Type *ty2) {
+  if (ty1->base)
+    return pointer_to(ty1->base);
+  if (ty1->size == 8 || ty2->size == 8)
+    return ty_long;
+  return ty_int;
+}
+
+// 對於許多二元運算符，我們隱式提升操作數，以便
+// 兩個運算元具有相同的型別。 任何小於的整數類型
+// int 總是提升為 int。 如果一個操作數的類型較大
+// 與另一個相比（例如“long”與“int”），較小的操作數將
+// 被提升以與另一個匹配。
+//
+// 這個運算稱為「普通算術轉換」。
+static void usual_arith_conv(Node **lhs, Node **rhs) {
+  Type *ty = get_common_type((*lhs)->ty, (*rhs)->ty);
+  *lhs = new_cast(*lhs, ty);
+  *rhs = new_cast(*rhs, ty);
+}
+
 void add_type(Node *node) {
   if (!node || node->ty)
     return;
@@ -66,23 +86,37 @@ void add_type(Node *node) {
     add_type(n);
 
   switch (node->kind) {
+  case ND_NUM:
+    node->ty = (node->val == (int)node->val)? ty_int : ty_long;
+    return;
   case ND_ADD:
   case ND_SUB:
   case ND_MUL:
   case ND_DIV:
-  case ND_NEG:
+    usual_arith_conv(&node->lhs, &node->rhs);
     node->ty = node->lhs->ty;
     return;
+  case ND_NEG: {
+    Type *ty = get_common_type(ty_int, node->lhs->ty);
+    node->lhs = new_cast(node->lhs, ty);
+    node->ty = ty;
+    return;
+  }
+
   case ND_ASSIGN:
     if (node->lhs->ty->kind == TY_ARRAY)
       error_tok(node->lhs->tok, "not an lvalue");
+    if (node->lhs->ty->kind != TY_STRUCT)
+      node->rhs = new_cast(node->rhs, node->lhs->ty);
     node->ty = node->lhs->ty;
     return;
   case ND_EQ:
   case ND_NE:
   case ND_LT:
   case ND_LE:
-  case ND_NUM:
+    usual_arith_conv(&node->lhs, &node->rhs);
+    node->ty = ty_int;
+    return;
   case ND_FUNCALL:
     node->ty = ty_long;
     return;
