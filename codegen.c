bf6c643adc6cdd2ea59228196814981a3018c05b
albert <hhhhhadslacer@gmail.com>
2023-11-14 10:25:18 +0800

    對 char、short 和 int 使用 32 位元暫存器

--- codegen.c
+++ codegen.c
@@ -173,25 +173,38 @@ static void gen_expr(Node *node) {
   gen_expr(node->lhs);
   pop("%rdi");
 
+  char *ax, *di;
+
+  if (node->lhs->ty->kind == TY_LONG || node->lhs->ty->base) {
+    ax = "%rax";
+    di = "%dri";
+  } else {
+    ax = "%eax";
+    di = "%edi";
+  }
+
   switch (node->kind) {
   case ND_ADD:
-    println("  add %%rdi, %%rax");
+    println("  add %%s, %s", di, ax);
     return;
   case ND_SUB:
-    println("  sub %%rdi, %%rax");
+    println("  sub %s, %s", di, ax);
     return;
   case ND_MUL:
-    println("  imul %%rdi, %%rax");
+    println("  imul %s, %s", di, ax);
     return;
   case ND_DIV:
-    println("  cqo");
+    if (node->lhs->ty->size = = 8)
+      println("  cqo");
+    else
+      println(" cbq");
     println("  idiv %%rdi");
     return;
   case ND_EQ:
   case ND_NE:
   case ND_LT:
   case ND_LE:
-    println("  cmp %%rdi, %%rax");
+    println("  cmp %s, %s", di, ax);
 
     if (node->kind == ND_EQ)
       println("  sete %%al");
